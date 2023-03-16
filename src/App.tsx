68dc54e6eae49ba493dd4268fb1a942ce7b79d26
Jchan <gkswocks123@gmail.com>
2023-03-16 21:39:39 +0900

    ✨ Feat : router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { AppRoutes } from "./components/AppRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MuiThemeProvider } from "./components/providers/MuiThemeProvider";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <MuiThemeProvider>
+          <AppRoutes />
+        </MuiThemeProvider>
+      </Router>
+    </>
+  );
 }
 
 export default App;
