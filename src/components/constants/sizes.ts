75e490663968a4ca40961d8156d0f535a247d515
Jchan <gkswocks123@gmail.com>
2023-03-18 21:29:54 +0900

    💄 Style : inputUploadImage UI 수정

--- src/components/constants/sizes.ts
+++ src/components/constants/sizes.ts
@@ -1,5 +1,14 @@
 import { breakPoints } from "./breakPoints";
 
+const availHeight = window.screen.availHeight;
+
+const inputUploadImage = {
+  outerWidth: availHeight * 0.3,
+  outerHeight: availHeight * 0.4,
+  innerWidth: availHeight * 0.3 * 0.9,
+  innerHeight: availHeight * 0.3 * 0.9,
+};
+
 /**
  * @description
  * Specify if component sizes are used in duplicate
@@ -7,4 +16,5 @@ import { breakPoints } from "./breakPoints";
 
 export const sizes = {
   pageMaxWidth: breakPoints.md - 500,
+  inputUploadImage,
 };
--- src/components/organisms/InputUploadImage.tsx
+++ src/components/organisms/InputUploadImage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography } from "@mui/material";
 import { appColor } from "../constants/appColor";
 import { Empty, FolderImage } from "../atoms";
+import { sizes } from "../constants/sizes";
 
 export const InputUploadImage = () => {
   // [Todo] 잘못된 이미지가 올라온 경우, 이미지 업로드에 실패한 경우
@@ -15,20 +16,19 @@ export const InputUploadImage = () => {
 
     setUploadedFile(fileList[0]);
   };
-
   return (
     // [Todo] 업로드에 따라 라벨 스타일도 변경 필요
     <label
       style={{
         outline: !!uploadedFile ? `1px solid black` : `4px dotted ${appColor.border}`,
         borderRadius: !!uploadedFile ? 0 : 20,
-        width: 200,
-        height: 300,
+        width: sizes.inputUploadImage.outerWidth,
+        height: sizes.inputUploadImage.outerHeight,
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        boxShadow: !!uploadedFile ? "0px 10px 5px rgba(0, 0, 0, 0.25)" : "",
+        boxShadow: !!uploadedFile ? "0px 5px 5px rgba(0, 0, 0, 0.25)" : "",
       }}
     >
       {!!uploadedFile ? <AfterUploadUI file={uploadedFile} /> : <PreUploadUI />}
@@ -54,7 +54,12 @@ const AfterUploadUI = ({ file }: { file: File }) => {
   return (
     // [Todo] 이미지 필터, 테마 적용 후 보여주어야 함
     <>
-      <div style={{ width: 180 }}>
+      <div
+        style={{
+          width: sizes.inputUploadImage.innerWidth,
+          height: sizes.inputUploadImage.innerHeight,
+        }}
+      >
         <img src={src} alt={file.name} style={{ width: "100%" }} />
       </div>
       <Empty height="2rem" />
