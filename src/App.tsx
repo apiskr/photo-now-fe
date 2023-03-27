6183f8f686af553147d7bf868120738799d87cce
Jchan <gkswocks123@gmail.com>
2023-03-27 14:24:17 +0900

    ✨ Feat : @tanstack/react-query provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { AppRoutes } from "./components/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { MuiThemeProvider } from "./components/providers/MuiThemeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <Router>
-        <MuiThemeProvider>
-          <AppRoutes />
-        </MuiThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <MuiThemeProvider>
+            <AppRoutes />
+          </MuiThemeProvider>
+        </QueryClientProvider>
       </Router>
     </>
   );
